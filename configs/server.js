86b1cd199d22d70548f46defa99301bd965b303d
ethan <ethanjupi@gmail.com>
2025-02-21 11:05:13 -0600

    build server

---
+++ configs/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import { dbConnection } from "./mongo.js";
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan("dev"))
+}
+
+const conectarDB = async () =>{
+    try{
+        await dbConnection()
+    }catch(err){
+        console.log(`Database connection failed: ${err}`)
+        process.exit(1)
+    }
+}
+
+export const initServer = () => {
+    const app = express()
+    try{
+        middlewares(app)
+        conectarDB()
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running on port ${process.env.PORT}`)
+    }catch(err){
+        console.log(`Server init failed: ${err}`)
+    }
+}
